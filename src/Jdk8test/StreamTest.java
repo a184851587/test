595482f48ed543e6a84b066f0b4b2fe163306f07
zhangdi <184851587@qq.com>
2018-04-08 16:53:51 +0800

    练习

--- src/Jdk8test/StreamTest.java
+++ src/Jdk8test/StreamTest.java
@@ -1,7 +1,6 @@
 package Jdk8test;
 
 import entity.Emp;
-import exception.BizCoreRuntimeException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,6 +20,12 @@ public class StreamTest {
         emps.add(new Emp(4, "lisi", 15));
 
 
+        List<Integer> ids = emps.stream().map(Emp::getId).collect(Collectors.toList());
+        List<String> names = emps.stream().map(Emp::getName).collect(Collectors.toList());
+        ids.forEach(e -> System.out.println(e));
+        names.forEach(n -> System.out.println(n));
+
+        System.out.println("--------------------------------------------");
 
         //filter 筛选list 中的值
         List<Emp> list = emps.stream()
@@ -62,7 +67,7 @@ public class StreamTest {
                 reduce("", String::concat);
         //sorted 进行排序 limit 取前n个 skip扔掉前n个元素
         System.out.println("sorted 进行排序 limit 取前n个 skip 取后n个");
-        emps.stream().sorted((e1,e2)->e1.getId().compareTo(e2.getId()))
+        emps.stream().sorted((e1, e2) -> e1.getId().compareTo(e2.getId()))
                 .limit(3)
                 .skip(1)
                 .forEach(emp -> System.out.println(emp));
@@ -70,6 +75,11 @@ public class StreamTest {
         System.out.println("max ,min 取最大值 最小值 返回");
         System.out.println(emps.stream().max(Comparator.comparing(Emp::getId)).orElseThrow(RuntimeException::new));
 
+        Arrays.asList("a1", "a2", "a3")
+                .stream()
+                .map(String::toUpperCase)
+                .findFirst()
+                .ifPresent(System.out::println);
 
     }
 
